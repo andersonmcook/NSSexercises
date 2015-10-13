e87e2bfa9ae7788a215f766c11bb421546c48a76
Anderson Cook <andersonmcook@gmail.com>
2015-10-13 10:18:19 -0500

    added some console.logs

--- boybands/boybands.js
+++ boybands/boybands.js
@@ -1,24 +1,27 @@
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
-console.log(bands);
+console.log("bands", bands);
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
-console.log(vegetables);
+console.log("vegetables", vegetables);
 // The number of loops to perform (what if the array changes?)
 var loopCount = bands.length; //+ vegetables.length;
-
+console.log("loopCount", loopCount);
 // Keep track of which band we're on in the loop
 var currentBand = bands[i];
-
+console.log("currentBand", currentBand);
 // Keep track of which veggie we're on in the loop
 var currentVeggie = vegetables[i];
-
+console.log("currentVeggie", currentVeggie);
 // Get a reference to the appropriate DOM element for bands
 var bandElement = document.getElementById("boy-bands");
-
+console.log("bandElement", bandElement);
 // Get a reference to the appropriate DOM element for vegetables
 var veggieElement = document.getElementById("vegetables");
+console.log("veggieElement", veggieElement);
 
 var outputBands = "";
+console.log("outputBands", outputBands);
 var outputVeggies = "";
+console.log("outputVeggies", outputVeggies);
 
 // Start looping
 for (var i = 0; i < loopCount; i += 1) {
@@ -38,6 +41,7 @@ for (var i = 0; i < loopCount; i += 1) {
 
   bandElement.innerHTML = outputBands;
   veggieElement.innerHTML = outputVeggies;
-console.log(bandElement);
+console.log("bandElement", bandElement);
+console.log("veggieElement", veggieElement);
 //Loop through the two arrays provided and output each element in the arrays into their corresponding
 // HTML <div> element. Ensure that each item is in a block element (e.g. li, div, p. etc...)
