95874f505e3e88f8ed8b535825bbddfa66e3da2f
Anderson Cook <andersonmcook@gmail.com>
2015-10-13 10:21:56 -0500

    added more console.logs

--- boybands/boybands.js
+++ boybands/boybands.js
@@ -3,14 +3,12 @@ console.log("bands", bands);
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 console.log("vegetables", vegetables);
 // The number of loops to perform (what if the array changes?)
-var loopCount = bands.length; //+ vegetables.length;
+var loopCount = (bands.length + vegetables.length) / 2;
 console.log("loopCount", loopCount);
 // Keep track of which band we're on in the loop
 var currentBand = bands[i];
-console.log("currentBand", currentBand);
 // Keep track of which veggie we're on in the loop
 var currentVeggie = vegetables[i];
-console.log("currentVeggie", currentVeggie);
 // Get a reference to the appropriate DOM element for bands
 var bandElement = document.getElementById("boy-bands");
 console.log("bandElement", bandElement);
@@ -28,19 +26,19 @@ for (var i = 0; i < loopCount; i += 1) {
 
   // Add the band names into the correct <div>
   var currentBand = bands[i];
-
+  console.log("currentBand", currentBand);
   // Add the veggie names into the correct <div>
   //currentVeggie = ???;
   var currentVeggie = vegetables[i];
-
+  console.log("currentVeggie", currentVeggie);
   outputBands = outputBands + "<div>" + currentBand + "</div>";
   outputVeggies = outputVeggies + "<div>" + currentVeggie + "</div>";
   //output = output + currentVeggie = "<div>" + currentVeggie + "</div>";
 
 }
 
-  bandElement.innerHTML = outputBands;
-  veggieElement.innerHTML = outputVeggies;
+bandElement.innerHTML = outputBands;
+veggieElement.innerHTML = outputVeggies;
 console.log("bandElement", bandElement);
 console.log("veggieElement", veggieElement);
 //Loop through the two arrays provided and output each element in the arrays into their corresponding
